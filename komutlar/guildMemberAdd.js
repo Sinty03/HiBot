fffc75244fb9f671f07f8b0e0d2012db4402ac82
Sinty03 <40046737+Sinty03@users.noreply.github.com>
2018-06-12 08:02:00 +0300

    Delete guildMemberAdd.js

--- komutlar/guildMemberAdd.js
+++
@@ -1,6 +0,0 @@
-const Discord = require('discord.js')
-module.exports = member => {
-    let username = member.user.username;
-    member.sendMessage('Aramıza hoşgeldin :maple_leaf: :dolphin:  **' + username + '** #kurallar kanalına bakmayı unutma. Umarım keyifli vakit geçirirsin. :smile:');
-    member.guild.defaultChannel.send('Aramıza hoşgeldin :fireworks: :rocket: :fireworks:  **' + username + '** herhangi bir konuda bir şey sormak istersen @Sınty#5397 ye dm atabilirsin :HiOWEN: :HiOWEN: :HiOWEN:  ');
-};
