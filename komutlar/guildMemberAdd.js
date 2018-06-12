e07d7cd1bbf88b5d7de1e73a62f9b007adb10a78
Sinty03 <40046737+Sinty03@users.noreply.github.com>
2018-06-12 08:01:16 +0300

    Update guildMemberAdd.js

--- komutlar/guildMemberAdd.js
+++ komutlar/guildMemberAdd.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js')
 module.exports = member => {
     let username = member.user.username;
     member.sendMessage('Aramıza hoşgeldin :maple_leaf: :dolphin:  **' + username + '** #kurallar kanalına bakmayı unutma. Umarım keyifli vakit geçirirsin. :smile:');
