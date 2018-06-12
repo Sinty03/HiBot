1e773f1a5b863eaf0734c7e7fd78c73ee249c606
Sinty03 <40046737+Sinty03@users.noreply.github.com>
2018-06-12 20:34:51 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,5 @@
 module.exports = member => {
   const channel = member.guild.channels.find('name', 'kırmızı-halı');
   if (!channel) return;
-  channel.send(`**📥 |** ${member} ** Aramıza hoşgeldin :tada: :hugging: :tada: :video_game: umarım keyifli vakit geçirirsin. Aklına taklına bir şey olursa :thinking: @Sınty#5397 ye mesaj göndererek sorularını sorabilirsin :innocent: .**`);
+  channel.send(`**📥 |** ${member} ** Aramıza hoşgeldin :tada: :hugging: :tada: :video_game: umarım keyifli vakit geçirirsin. Aklına taklına bir şey olursa :thinking: @Sınty ye mesaj göndererek sorularını sorabilirsin :innocent: .**`);
 };
