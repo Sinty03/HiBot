8497a1dc78bfac6f3e81dac5b2a9d8e84dce3d6e
Sinty03 <40046737+Sinty03@users.noreply.github.com>
2018-06-12 20:37:37 +0300

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -2,5 +2,5 @@ module.exports = member => {
   const channel = member.guild.channels.find('name', 'ölüler');
   if (!channel) return;
   member.send(`Niye gittin ? https://www.youtube.com/watch?v=SLNgTfuJssw`)
-  channel.send(`**Sus ** ${member} ** yok ** ${member} ** öldü serverden ayrıldığı gün o iş bitti. **`);
+  channel.send(`**Sus. ** ${member} ** yok. ** ${member} ** öldü serverden ayrıldığı gün o iş bitti. **`);
 };
